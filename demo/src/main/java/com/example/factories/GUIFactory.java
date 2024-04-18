fb4a72584369dbb0db1242e9dbf00dc5cae0a443
andreidavid8152 <76012933+andreidavid8152@users.noreply.github.com>
2024-04-18 00:28:12 +0000

    Implementacion Abstract Factory

---
+++ demo/src/main/java/com/example/factories/GUIFactory.java
@@ -0,0 +1,10 @@
+package com.example.factories;
+
+import com.example.buttons.Button;
+import com.example.checkboxes.Checkbox;
+
+public interface GUIFactory {
+    Button createButton();
+
+    Checkbox createCheckbox();
+}
